41d60f632d106ad8d018e1e0c1fdb110eeb72cab
wswjuea <330669255@qq.com>
2019-10-31 13:37:03 +0800

    测试2

--- app/__init__.py
+++ app/__init__.py
@@ -15,10 +15,10 @@ app.config["FC_DIR"] = os.path.join(os.path.abspath(os.path.dirname(__file__)), 
 app.debug = True
 db = SQLAlchemy(app)
 
-from app.home import home as home_blueprint
+# from app.home import home as home_blueprint
 from app.admin import admin as admin_blueprint
 
-app.register_blueprint(home_blueprint)
+# app.register_blueprint(home_blueprint)
 app.register_blueprint(admin_blueprint, url_prefix="/admin")
 
 
